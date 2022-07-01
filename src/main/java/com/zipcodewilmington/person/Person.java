b3a598fba040903e326cd739bf990c33c704095e
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-01 17:03:27 -0400

    fields added & tested

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -5,31 +5,102 @@ package com.zipcodewilmington.person;
  */
 public class Person {
     private String name;
-    private int age;
+    private Integer age;
+
+    //added fields
+    private Integer heightInCm;
+    private String gender;
+    private String streetWhereYouGrewUp;
+    private String mothersMaidenName;
+    private String firstPetsName;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.gender = ":eyes:";
+        this.heightInCm = 180;
+        this.streetWhereYouGrewUp = "Easy Street";
+        this.mothersMaidenName = "please";
+        this.firstPetsName = "REX";
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age, String gender, Integer height, String street, String maiden, String pet) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.heightInCm = height;
+        this.streetWhereYouGrewUp = street;
+        this.mothersMaidenName = maiden;
+        this.firstPetsName = pet;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setHeightInCm(int height) {
+        this.heightInCm = height;
+    }
+
+    public void setStreetWhereYouGrewUp(String street) {
+        this.streetWhereYouGrewUp = street;
+    }
+
+    public void setMothersMaidenName(String maiden) {
+        this.mothersMaidenName = maiden;
+    }
+
+    public void setFirstPetsName(String pet) {
+        this.firstPetsName = pet;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getHeightInCm() {
+        return heightInCm;
+    }
+
+    public String getStreetWhereYouGrewUp() {
+        return streetWhereYouGrewUp;
+    }
+
+    public String getMothersMaidenName() {
+        return mothersMaidenName;
+    }
+
+    public String getFirstPetsName() {
+        return firstPetsName;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,11 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        String expectedGender = ":eyes:";
+        Integer expectedHeight = 180;
+        String expectedStreet = "Easy Street";
+        String expectedMaiden = "please";
+        String expectedPetName = "REX";
 
         // When
         Person person = new Person();
@@ -19,9 +24,19 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        String actualGender = person.getGender();
+        Integer actualHeight = person.getHeightInCm();
+        String actualStreet = person.getStreetWhereYouGrewUp();
+        String actualMaiden = person.getMothersMaidenName();
+        String actualPetName = person.getFirstPetsName();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedMaiden, actualMaiden);
+        Assert.assertEquals(expectedStreet, actualStreet);
+        Assert.assertEquals(expectedPetName, actualPetName);
     }
 
     @Test
@@ -69,6 +84,39 @@ public class TestPerson {
     }
 
     @Test
+    public void testCompleteConstructor() {
+        // Given
+        String expectedName = "Olas";
+        Integer expectedAge = 35;
+        String expectedGender = "male";
+        Integer expectedHeight = 180;
+        String expectedStreet = "Clay Street";
+        String expectedMaiden = "uh";
+        String expectedPetName = "Jaws";
+
+        // When
+        Person person = new Person("Olas", 35, "male", 180, "Clay Street", "uh", "Jaws");
+
+        // Then
+        String actualName = person.getName();
+        Integer actualAge = person.getAge();
+        String actualGender = person.getGender();
+        Integer actualHeight = person.getHeightInCm();
+        String actualStreet = person.getStreetWhereYouGrewUp();
+        String actualMaiden = person.getMothersMaidenName();
+        String actualPetName = person.getFirstPetsName();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedMaiden, actualMaiden);
+        Assert.assertEquals(expectedStreet, actualStreet);
+        Assert.assertEquals(expectedPetName, actualPetName);
+    }
+
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -95,4 +143,75 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetGender() {
+        // Given
+        Person person = new Person();
+        String expected = "ambiguous";
+
+        // When
+        person.setGender(expected);
+
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setHeightInCm(expected);
+
+        // Then
+        Integer actual = person.getHeightInCm();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetStreet() {
+        // Given
+        Person person = new Person();
+        String expected = "Mockingbird Lane";
+
+        // When
+        person.setStreetWhereYouGrewUp(expected);
+
+        // Then
+        String actual = person.getStreetWhereYouGrewUp();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetMaiden() {
+        // Given
+        Person person = new Person();
+        String expected = "Nunya";
+
+        // When
+        person.setMothersMaidenName(expected);
+
+        // Then
+        String actual = person.getMothersMaidenName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPet() {
+        // Given
+        Person person = new Person();
+        String expected = "Rover Dangerfield";
+
+        // When
+        person.setFirstPetsName(expected);
+
+        // Then
+        String actual = person.getFirstPetsName();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
